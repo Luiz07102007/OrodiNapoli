64f4dbfae96cec1dff64705087ac9abc59945172
Luiz07102007 <luiz.rodrigues198@etec.sp.gov.br>
2024-05-29 20:14:38 -0300

    Initial commit

---
+++ app/src/main/java/com/example/orodinapoli/SplashScreen2.java
@@ -0,0 +1,43 @@
+package com.example.orodinapoli;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.VideoView;
+import android.net.Uri;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashScreen2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash_screen2);
+        VideoView videoView = findViewById(R.id.videopizza);
+        Uri videoUri= Uri.parse("android.resource://" +
+                getPackageName() + "/" +
+                R.raw.pizzaquente);
+        videoView.setVideoURI(videoUri);
+        videoView.start();
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen2.this,
+                        Login1.class);
+                startActivity(intent);
+            }
+        },2500);
+    }
+}
