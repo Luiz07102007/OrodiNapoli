c80842fe33479aca4d928960656cd0189a62a880
Luiz07102007 <luiz.rodrigues198@etec.sp.gov.br>
2024-05-31 11:55:01 -0300

    Arrumei essa porcaria de cardapio

--- app/src/main/java/com/example/orodinapoli/cardapio.java
+++ app/src/main/java/com/example/orodinapoli/cardapio.java
@@ -1,7 +1,6 @@
 package com.example.orodinapoli;
 
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,9 +15,6 @@ public class cardapio extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_cardapio);
-        View decorView = getWindow().getDecorView();
-        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
-        decorView.setSystemUiVisibility(uiOptions);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
