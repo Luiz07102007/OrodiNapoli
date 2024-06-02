7155e45da5d72d376be39898be428f1d8003eb52
Luiz07102007 <luiz.rodrigues198@etec.sp.gov.br>
2024-06-01 23:24:10 -0300

    tentativa de tema2

--- app/src/main/java/com/example/orodinapoli/config.java
+++ app/src/main/java/com/example/orodinapoli/config.java
@@ -2,6 +2,7 @@ package com.example.orodinapoli;
 
 import android.app.UiModeManager;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,6 +12,7 @@ import android.widget.RadioGroup;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -24,32 +26,26 @@ public class config extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_config);
-        UiModeManager uiModeManager = (UiModeManager) getSystemService(UI_MODE_SERVICE);
 
-        // Configurar RadioGroup e RadioButtons
-        radio_group = findViewById(R.id.rgtema);
         claro = findViewById(R.id.rbmodoclaro);
         escuro = findViewById(R.id.rbmodoescuro);
 
-        // Verificar o modo atual e definir o RadioButton correspondente
-        int currentMode = getResources().getConfiguration().uiMode & android.content.res.Configuration.UI_MODE_NIGHT_MASK;
-        if (currentMode == android.content.res.Configuration.UI_MODE_NIGHT_YES) {
-            escuro.setChecked(true);
-        } else {
-            claro.setChecked(true);
-        }
 
-        radio_group.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+        claro.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCheckedChanged(RadioGroup group, int checkedId) {
-                if (checkedId == R.id.rbmodoclaro) {
-                    uiModeManager.setNightMode(UiModeManager.MODE_NIGHT_NO);
-                } else if (checkedId == R.id.rbmodoescuro) {
-                    uiModeManager.setNightMode(UiModeManager.MODE_NIGHT_YES);
-                }
-                recreate();
+            public void onClick(View view) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
             }
         });
+
+        escuro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            }
+        });
+
+
         Button home = findViewById(R.id.bttelainicial);
         home.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,7 +54,6 @@ public class config extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
 
         View decorView = getWindow().getDecorView();
         int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
